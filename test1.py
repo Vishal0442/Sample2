723ca0158125a2b3dd8659dbbf45afc284eaebb2
Vishal Farakate <vishal1517@gmail.com>
2021-01-09 23:34:01 +0530

    Some more content added in both files.

--- test1.py
+++ test1.py
@@ -5,3 +5,8 @@ def fun():
 	return "5 + 2 = {}".format(7)
 
 print("sample add function --> ", fun())
+
+def gun():
+	return "5 - 2 = {}".format(3)
+
+print("sample subtraction function --> ", gun())
