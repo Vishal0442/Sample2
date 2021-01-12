26ce94690a56dafb2e64d24c4b1dc38a54afe117
Vishal Farakate <vishal1517@gmail.com>
2021-01-12 23:42:46 +0530

    added division function

--- test1.py
+++ test1.py
@@ -4,12 +4,15 @@ print("This is test1.py file")
 def addition():
 	return "5 + 2 = {}".format(7)
 
-print("sample add function --> ", fun())
+print("sample add function --> ", addition())
 
 def substraction():
 	return "5 - 2 = {}".format(3)
 
-print("sample subtraction function --> ", gun())
+print("sample subtraction function --> ", substraction())
 
-def sun():
-	pass
+def division():
+	return "5 / 2 = {}".format(2.5)
+
+print("sample subtraction function --> ", division())
+
